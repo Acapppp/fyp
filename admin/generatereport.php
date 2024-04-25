65719032a67777bf9510e3f3599b9df12e9ff940
shamim0387 <144081099+shamim0387@users.noreply.github.com>
2024-04-25 22:36:22 +0800

    Update generatereport.php

--- admin/generatereport.php
+++ admin/generatereport.php
@@ -2,7 +2,7 @@
 include('database/connection.php');
 
 $display = "SELECT custname, custic, custphone, payment, price, regdate 
-            FROM custinfo";
+            FROM report";
 
 $resultdis = $con->query($display);
 
