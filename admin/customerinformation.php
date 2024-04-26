0daa35241542cdf16cb4a87d5b9cec0c8b1345ff
Acapppp <143793552+Acapppp@users.noreply.github.com>
2024-04-27 03:02:38 +0800

    Update customerinformation.php

--- admin/customerinformation.php
+++ admin/customerinformation.php
@@ -32,7 +32,6 @@ $resultdis = $con->query($display);
     <title>Customer Information | Baam Gadget</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
-    
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     <link rel="stylesheet" href="admin.css">
 </head>
